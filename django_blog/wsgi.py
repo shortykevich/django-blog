cfa7d7113c4014eede8abe4a71d575d758cee20b
Kirill Dvoretsky <shortykofficial@gmail.com>
2024-08-09 14:55:24 +0300

    Initial commit

---
+++ django_blog/wsgi.py
@@ -0,0 +1,21 @@
+"""
+WSGI config for django_blog project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_blog.settings')
+
+application = get_wsgi_application()
+
+
+def hello_world(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return ['Hello World!']
