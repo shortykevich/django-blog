2781c0de5da23d9d25cb159aac0eb71ba64c4546
Kirill Dvoretsky <shortykofficial@gmail.com>
2024-08-12 22:10:28 +0300

    Little naming fixes plus learned a bit about how to create-add-find-filter models instances via ORM interface

--- django_blog/articles/models.py
+++ django_blog/articles/models.py
@@ -5,4 +5,4 @@ class Article(models.Model):
     name = models.CharField(max_length=200)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
