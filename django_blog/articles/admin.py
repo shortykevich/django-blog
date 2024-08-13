dd52f1144f8944015a360e5ffb8692bc5e10097f
Kirill Dvoretsky <shortykofficial@gmail.com>
2024-08-13 16:55:41 +0300

    Learned basics about Django Admin

--- django_blog/articles/admin.py
+++ django_blog/articles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ['name', 'body']
+    list_filter = (('created_at', admin.DateFieldListFilter),)
